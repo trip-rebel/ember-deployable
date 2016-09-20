54797e4cc09ffa109f4bee6a14ff95e26135be81
Pepijn Schoen <pepijn@eliandor.com>
2016-09-20 15:48:26 +0200

    Initial commit

---
+++ TREmberDeployable/Controllers/IndexController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.WindowsAzure.Storage;
+using System;
+using Microsoft.WindowsAzure.Storage.Table;
+using System.Threading.Tasks;
+
+namespace TREmberDeployable.Controllers
+{
+    public class IndexController : Controller
+    {
+        private string ApplicationKey;
+        private CloudTable StorageTable;
+        
+        public IndexController()
+        {
+            var storageConnectionString = Environment.GetEnvironmentVariable("TABLESTORAGE_CONNECTIONSTRING");
+            var storageTableName = Environment.GetEnvironmentVariable("TABLESTORAGE_TABLENAME");
+
+            ApplicationKey = Environment.GetEnvironmentVariable("EMBER_APP_KEY");
+
+            var storageAccount = CloudStorageAccount.Parse(storageConnectionString);
+            var tableClient = storageAccount.CreateCloudTableClient();
+
+            StorageTable = tableClient.GetTableReference(storageTableName);
+        }
+
+        [HttpGet("{*url}")]
+        public async Task<IActionResult> Get(string version = null)
+        {
+            await StorageTable.CreateIfNotExistsAsync();
+
+            if(string.IsNullOrEmpty(version))
+            {
+                version = await GetCurrentVersion();
+            }
+
+            var content = await Find(version);
+
+            return Content(content, "text/html");
+        }
+
+        private async Task<string> GetCurrentVersion()
+        {
+            return await Find($"{ApplicationKey}:current");
+        }
+
+        private async Task<string> Find(string key)
+        {
+            var result = (DynamicTableEntity)(await StorageTable.ExecuteAsync(TableOperation.Retrieve("manifest", key))).Result;
+
+            return result.Properties["content"].StringValue;
+        }
+    }
+}
---
+++ TREmberDeployable/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Builder;
+
+namespace TREmberDeployable
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseUrls("http://*:5000/")
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
